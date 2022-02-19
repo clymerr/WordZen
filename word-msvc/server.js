eaeeb5c44b91f86d860f854e0a85ee60c62f9937
clymerr <clymerr@gmail.com>
2022-02-18 21:41:35 -0500

    Microservice initial version complete - supports /words and /phrase

--- word-msvc/server.js
+++ word-msvc/server.js
@@ -1,34 +1,63 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
+let app = express();
 app.set('port', 4758);
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const https = require('https')
 
 
 app.get('/phrases', function(req, res, next) {
-    res.send(JSON.stringify(x));
+  callDatamuse(req.query.mode, req.query.input).then((result) => {
+    res.json(result);
+  })
 });
 
 app.get('/words', function(req, res, next) {
+  let mode = req.query.mode;
+  let words = req.query.input.replace(/[^\w\s]/gi, '').split(' ');
 
-  res.send(JSON.stringify(x));
+  let promises = []
+  for (const element of words) {
+    promises.push(callDatamuse(mode, element));
+  }
+  
+  Promise.all(promises).then((result) => {
+    res.json([].concat.apply([], result))
+  });
 });
 
 app.use(function(req,res){
     res.type('text/plain');
     res.status(404);
     res.send('404 - No Page Here!');
-  });
+});
   
-  app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.type('plain/text');
-    res.status(500);
-    res.send('500 - Server Error');
-  });
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+  console.log(`Express started on http://localhost:${app.get('port')}; press Ctrl-C to terminate.`);
+});
+
+function callDatamuse(mode, input) {
+  return new Promise((resolve, reject) => {
+    https.get(`https://api.datamuse.com/words?${mode}=${input}`, (resp) => {
+      let data = '';
   
-  app.listen(app.get('port'), function(){
-    console.log(`Express started on http://${process.env.HOSTNAME}:${app.get('port')}; press Ctrl-C to terminate.`);
+      resp.on('data', (chunk) => {
+        data += chunk;
+      });
+  
+      resp.on('end', () => {
+        resolve(JSON.parse(data));
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error: " + err.message);
+    });
   });
+}
